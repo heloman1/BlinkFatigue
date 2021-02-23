3d68aee15d2e47f6d909b93277911b4243797966
Edward Gomez <2865652+heloman1@users.noreply.github.com>
2021-02-22 21:14:24 -0500

    Added Config Comments

--- Config.cs
+++ Config.cs
@@ -9,11 +9,22 @@ namespace BlinkFatigue
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; }
 
+        [Description("The rate at which the cooldown of the blink speed is applied. Explained further in the GitHub page.")]
         public float DecreaseRate { get; set; } = 0.75f;
+
+        [Description("⚠ Making people blink too fast may cause epileptic seizures. Change this config at your own risk. ⚠")]
         public float MinBlinkTime { get; set; } = 1.5f;
+
+        [Description("The minimum time between blinks. Explained further in the GitHub page.")]
         public float MinTime { get; set; } = 2.5f;
+
+        [Description("The maximum time between blinks. Explained further in the GitHub page.")]
         public float MaxTime { get; set; } = 3.5f;
+
+        [Description("The minimum time to add to the rework value. Explained further in the GitHub page.")]
         public float AddMin { get; set; } = 0.35f;
+
+        [Description("The minimum time to add to the rework value. Explained further in the GitHub page.")]
         public float AddMax { get; set; } = 0.45f;
     }
 }
